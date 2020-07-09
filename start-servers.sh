9fd344dda00b44bd4fd934b142b00860ec727355
lex <emailforalexgo@gmail.com>
2020-07-09 01:29:44 -0400

    start servers

--- start-servers.sh
+++ start-servers.sh
@@ -1,4 +1,13 @@
 node --version
 
-node /api/main.js&
-node /app/server/server.js
+echo "ls"
+ls
+
+echo "ls api"
+ls api
+
+echo "ls app"
+ls app
+
+node ./api/main.js&
+node ./app/server/server.js
